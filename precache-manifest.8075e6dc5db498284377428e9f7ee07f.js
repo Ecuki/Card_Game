d465e7a9bb38469365e6045d6db227aced74f78e
Ecuki <31224521+Ecuki@users.noreply.github.com>
2020-04-22 19:20:38 +0200

    Updates

---
+++ precache-manifest.8075e6dc5db498284377428e9f7ee07f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c10809485cf2e888ff1e92ff45237c4",
+    "url": "/Card_Game/index.html"
+  },
+  {
+    "revision": "37a7c30979a6571063c2",
+    "url": "/Card_Game/static/css/2.2bd37e2f.chunk.css"
+  },
+  {
+    "revision": "26f25525bc54aca637ea",
+    "url": "/Card_Game/static/css/main.97fc37d0.chunk.css"
+  },
+  {
+    "revision": "37a7c30979a6571063c2",
+    "url": "/Card_Game/static/js/2.edff24e4.chunk.js"
+  },
+  {
+    "revision": "635ddcea1789cec178ae3d670f2bc739",
+    "url": "/Card_Game/static/js/2.edff24e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f25525bc54aca637ea",
+    "url": "/Card_Game/static/js/main.c1256c62.chunk.js"
+  },
+  {
+    "revision": "b480f891ddaa748e427b",
+    "url": "/Card_Game/static/js/runtime-main.faed3f06.js"
+  },
+  {
+    "revision": "1ea62f1643db62d7f5fc16e1f5fe82d7",
+    "url": "/Card_Game/static/media/Map.1ea62f16.png"
+  },
+  {
+    "revision": "30fd692572ede9a8b6dc31f08170a739",
+    "url": "/Card_Game/static/media/texture.30fd6925.png"
+  }
+]);
